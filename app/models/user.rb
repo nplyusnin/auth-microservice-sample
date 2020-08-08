33f955e3521cc4f626a29a96b3b314379616eccd
Никита Плюснин <nplyusnin14@gmail.com>
2020-08-08 18:50:43 +0300

    added name and email presence validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < Sequel::Model
-
+  def validate
+    super
+    validates_presence :name,  message: I18n.t(:blank, scope: 'model.errors.user.name')
+    validates_presence :email, message: I18n.t(:blank, scope: 'model.errors.user.email')
+  end
 end
